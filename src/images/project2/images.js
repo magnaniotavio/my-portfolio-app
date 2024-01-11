5a9af11e794b5a066c189a71aa63a43ef4893288
otavio_dm <102213097+magnaniotavio@users.noreply.github.com>
2024-01-11 05:40:43 -0300

    Updates

--- src/images/project2/images.js
+++
@@ -1,3 +0,0 @@
-export { default as image1 } from './image1.png';
-export { default as image2 } from './image2.png';
-export { default as image3 } from './image3.png';
--- src/images/project4/images.js
+++
@@ -1,4 +0,0 @@
-export { default as climateimg1 } from './climateimg1.png';
-export { default as climateimg2 } from './climateimg2.png';
-export { default as climateimg3 } from './climateimg3.png';
-export { default as climateimg4 } from './climateimg4.png';
